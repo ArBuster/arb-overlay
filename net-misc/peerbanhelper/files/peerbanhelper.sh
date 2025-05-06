2875de065c986656d290bcdf04e547e4ec553a77
ArBuster <you@example.com>
2025-05-06 11:41:42 +0800

    修改：     net-misc/peerbanhelper/files/peerbanhelper.sh

--- net-misc/peerbanhelper/files/peerbanhelper.sh
+++ net-misc/peerbanhelper/files/peerbanhelper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #if [ `whoami` == "root" ]; then
-    /usr/bin/java -Dpbh.datadir=/var/lib/peerbanhelper -Dpbh.configdir=/etc/peerbanhelper -Dpbh.logsdir=/var/log/peerbanhelper -Dpbh.log.level=WARN -Xmx512M -Xms16M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -jar PeerBanHelper.jar
+    /usr/bin/java -Dpbh.datadir=/var/lib/peerbanhelper -Dpbh.configdir=/etc/peerbanhelper -Dpbh.logsdir=/var/log/peerbanhelper -Dpbh.log.level=WARN -Xmx512M -Xms16M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -jar /opt/peerbanhelper/PeerBanHelper.jar
 #else
 #    echo "Require root privilege."
 #fi
