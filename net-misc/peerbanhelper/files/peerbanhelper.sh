352149eef15ab2f9052a88cafbf6992bdbe50ce6
ArBuster <you@example.com>
2025-05-06 13:26:19 +0800

    修改：     net-misc/peerbanhelper/files/peerbanhelper.service 	修改：     net-misc/peerbanhelper/files/peerbanhelper.sh

--- net-misc/peerbanhelper/files/peerbanhelper.sh
+++ net-misc/peerbanhelper/files/peerbanhelper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/java -Dpbh.datadir=~/.local/share/peerbanhelper/data -Dpbh.configdir=/etc/peerbanhelper -Dpbh.logsdir=~/.local/share/peerbanhelper/log -Dpbh.log.level=WARN -Xmx512M -Xms16M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -jar /opt/peerbanhelper/PeerBanHelper.jar nogui
+/usr/bin/java -Dpbh.datadir=${HOME}/.local/share/peerbanhelper/data -Dpbh.configdir=/etc/peerbanhelper -Dpbh.logsdir=${HOME}/.local/share/peerbanhelper/log -Dpbh.log.level=WARN -Xmx512M -Xms16M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -jar /opt/peerbanhelper/PeerBanHelper.jar nogui
